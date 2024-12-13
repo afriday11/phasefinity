f5e195eeebb7cf63cdc4b0af6062fbb6941be420
Dan Fessler <fezzinate@gmail.com>
2024-12-13 02:36:41 -0800

    using audiocontext instead of audio elements

--- src/utils/playDealSound.ts
+++ src/utils/playDealSound.ts
@@ -1,26 +1,44 @@
 import cardDrawSound1 from "/dealcard1.mp3";
 import cardDrawSound2 from "/dealcard2.mp3";
-// import cardDrawSound3 from "/dealcard3.mp3";
 
-const soundPool: HTMLAudioElement[] = [];
-const soundPoolSize = 5;
-let currentSoundIndex = 0;
+const audioContext = new AudioContext();
+const soundBuffers: AudioBuffer[] = [];
 
-for (let i = 0; i < soundPoolSize; i++) {
+// Load and decode audio files
+async function loadSounds() {
   const sounds = [cardDrawSound1, cardDrawSound2];
-  const sound = new Audio(sounds[Math.floor(Math.random() * sounds.length)]);
-  soundPool.push(sound);
+
+  for (const soundUrl of sounds) {
+    const response = await fetch(soundUrl);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+    soundBuffers.push(audioBuffer);
+  }
 }
 
+// Initialize sounds
+loadSounds();
+
 function playSound() {
-  const sfx = soundPool[currentSoundIndex];
-  currentSoundIndex = (currentSoundIndex + 1) % soundPoolSize;
-  sfx.preservesPitch = false;
+  // Create buffer source
+  const source = audioContext.createBufferSource();
+  const gainNode = audioContext.createGain();
+
+  // Get random sound from our decoded buffers
+  const randomBuffer =
+    soundBuffers[Math.floor(Math.random() * soundBuffers.length)];
+  source.buffer = randomBuffer;
+
+  // Optional: Add variance to playback
   const varianceRange = 0.25;
   const variance = Math.random() * varianceRange - varianceRange / 2;
-  sfx.volume = 0.4 + variance;
-  sfx.playbackRate = 1 + variance;
-  sfx.play();
+  source.playbackRate.value = 1 + variance;
+  gainNode.gain.value = 0.4 + variance;
+
+  // Connect nodes and play
+  source.connect(gainNode);
+  gainNode.connect(audioContext.destination);
+  source.start();
 }
 
 export default playSound;
