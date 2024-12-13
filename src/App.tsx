657a985e8de4752b3dad32e9e76092f36f9f837a
Friday <afriday11@gmail.com>
2024-12-13 12:04:21 -0800

    Added score and hand evaluation logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { useReducer } from "react";
 import "./App.css";
 import GameControls from "./components/GameControls";
 import GameBoard from "./components/GameBoard";
+import ScoreDisplay from "./components/ScoreDisplay";
 import gameReducer, { State } from "./reducers/gameReducer";
+import scoreReducer, { initialScoreState } from "./reducers/scoreReducer";
 
 const initialState: State = {
   gameStarted: false,
@@ -10,31 +12,34 @@ const initialState: State = {
 };
 
 function App() {
-  const [state, dispatch] = useReducer(gameReducer, initialState);
+  const [gameState, gameDispatch] = useReducer(gameReducer, initialState);
+  const [scoreState, scoreDispatch] = useReducer(scoreReducer, initialScoreState);
 
   // the cards are in a single array so that React can animate them
   // so we need to filter them out into separate arrays for each position
-  const deckCards = state.cards.filter((card) => card.position === "deck");
-  const handCards = state.cards.filter((card) => card.position === "hand");
-  const boardCards = state.cards.filter((card) => card.position === "board");
-  const discardPile = state.cards.filter((card) => card.position === "discard");
-  const selectedCards = state.cards.filter((card) => card.selected);
+  const deckCards = gameState.cards.filter((card) => card.position === "deck");
+  const handCards = gameState.cards.filter((card) => card.position === "hand");
+  const boardCards = gameState.cards.filter((card) => card.position === "board");
+  const discardPile = gameState.cards.filter((card) => card.position === "discard");
+  const selectedCards = gameState.cards.filter((card) => card.selected);
 
   return (
     <div>
+      {gameState.gameStarted && <ScoreDisplay score={scoreState} />}
       <GameBoard
         handCards={handCards}
         boardCards={boardCards}
         discardPile={discardPile}
         deckCards={deckCards}
-        dispatch={dispatch}
+        dispatch={gameDispatch}
       />
       <GameControls
-        gameStarted={state.gameStarted}
+        gameStarted={gameState.gameStarted}
         handCards={handCards}
         selectedCards={selectedCards}
         deckCards={deckCards}
-        dispatch={dispatch}
+        dispatch={gameDispatch}
+        scoreDispatch={scoreDispatch}
       />
     </div>
   );
